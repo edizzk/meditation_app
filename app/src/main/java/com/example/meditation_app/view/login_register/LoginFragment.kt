cce30f86a1eaf15999d8de6de0be215f484abd12
Ediz Mete Kurt <edizmetekurt@gmail.com>
2022-10-18 11:58:05 +0300

    use observer in login fragment to check UiState types

--- app/src/main/java/com/example/meditation_app/view/login_register/LoginFragment.kt
+++ app/src/main/java/com/example/meditation_app/view/login_register/LoginFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.example.meditation_app.databinding.FragmentLoginBinding
+import com.example.meditation_app.utils.UiState
+import com.example.meditation_app.utils.toast
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -25,11 +27,27 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        observer()
         binding.loginButton.setOnClickListener {
             viewModel.login(
                 email = binding.emailEditText.text.toString(),
                 password = binding.passwordEditText.text.toString()
             )
+        }
+    }
+
+    private fun observer(){
+        viewModel.login.observe(viewLifecycleOwner) { state ->
+            when(state){
+                is UiState.Loading -> {
+                }
+                is UiState.Failure -> {
+                    toast(state.error)
+                }
+                is UiState.Success -> {
+                    toast(state.data)
+                }
+            }
         }
     }
 
