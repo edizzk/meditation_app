50ebfc132d599ef9d2f63268a5d305934da406ed
Ediz Mete Kurt <edizmetekurt@gmail.com>
2022-10-18 10:50:25 +0300

    set fragments for view pager

--- app/src/main/java/com/example/meditation_app/MainActivity.kt
+++ app/src/main/java/com/example/meditation_app/MainActivity.kt
@@ -11,8 +11,29 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val fragmentList = listOf(
+        RegisterFragment.create(),
+        LoginFragment.create()
+    )
+    private val fragmentTitleList = listOf(
+        "ÜYE OL",
+        "GİRİŞ YAP"
+    )
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val adapter = ViewPagerAdapter(this, fragmentList)
+        binding.viewPager2.adapter = adapter
+        TabLayoutMediator(binding.tabLayout, binding.viewPager2){
+            tab, pos -> tab.text = fragmentTitleList[pos]
+        }.attach()
+
     }
 }
