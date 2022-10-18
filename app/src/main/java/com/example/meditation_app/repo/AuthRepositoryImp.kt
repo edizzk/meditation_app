81a3bb99ccb9fb18dbf776abb20d3dfc943065e3
Ediz Mete Kurt <edizmetekurt@gmail.com>
2022-10-18 11:05:44 +0300

    use error handling when try to connect firebase auth

--- app/src/main/java/com/example/meditation_app/repo/AuthRepositoryImp.kt
+++ app/src/main/java/com/example/meditation_app/repo/AuthRepositoryImp.kt
@@ -3,9 +3,13 @@ package com.example.meditation_app.repo
 import com.example.meditation_app.model.User
 import com.example.meditation_app.utils.UiState
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
+import java.lang.Exception
 
 class AuthRepositoryImp(
-    private val auth: FirebaseAuth,
+    private val auth: FirebaseAuth
 ) : AuthRepository {
 
     override fun registerUser(
@@ -19,6 +23,18 @@ class AuthRepositoryImp(
                     result.invoke(
                         UiState.Success("User registered successfully!")
                     )
+                }else {
+                    try {
+                        throw it.exception ?: Exception("Invalid authentication")
+                    } catch (e: FirebaseAuthWeakPasswordException) {
+                        result.invoke(UiState.Failure("Authentication failed, password should be at least 6 characters"))
+                    } catch (e: FirebaseAuthInvalidCredentialsException) {
+                        result.invoke(UiState.Failure("Authentication failed, invalid email entered"))
+                    } catch (e: FirebaseAuthUserCollisionException) {
+                        result.invoke(UiState.Failure("Authentication failed, email already registered"))
+                    } catch (e: Exception) {
+                        result.invoke(UiState.Failure(e.message))
+                    }
                 }
             }
             .addOnFailureListener {
