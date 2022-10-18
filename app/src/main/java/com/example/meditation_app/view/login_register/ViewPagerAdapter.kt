96a1d7208be0fc9105a274da03543b37050bc1ff
Ediz Mete Kurt <edizmetekurt@gmail.com>
2022-10-18 10:55:43 +0300

    create view pager adapter

---
+++ app/src/main/java/com/example/meditation_app/view/login_register/ViewPagerAdapter.kt
@@ -0,0 +1,17 @@
+package com.example.meditation_app.view.login_register
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(fragmentActivity: FragmentActivity, private val list: List<Fragment>)
+    : FragmentStateAdapter(fragmentActivity) {
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return list[position]
+    }
+}
