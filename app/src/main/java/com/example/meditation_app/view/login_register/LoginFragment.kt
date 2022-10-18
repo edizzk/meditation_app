a7ba389d4a590e5de00d6cc9cff1b68818c24fa2
Ediz Mete Kurt <edizmetekurt@gmail.com>
2022-10-18 11:56:01 +0300

    get infos from login page and sent them to viewmodel login method

--- app/src/main/java/com/example/meditation_app/view/login_register/LoginFragment.kt
+++ app/src/main/java/com/example/meditation_app/view/login_register/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.meditation_app.databinding.FragmentLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,6 +13,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class LoginFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginBinding
+    private val viewModel: AuthViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +21,16 @@ class LoginFragment : Fragment() {
     ): View {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.loginButton.setOnClickListener {
+            viewModel.login(
+                email = binding.emailEditText.text.toString(),
+                password = binding.passwordEditText.text.toString()
+            )
+        }
     }
 
     companion object {
