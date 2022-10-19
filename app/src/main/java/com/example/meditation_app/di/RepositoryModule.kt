6ce5ea172043823039709fbb2ba9a7604f591a38
Ediz Mete Kurt <edizmetekurt@gmail.com>
2022-10-19 11:06:51 +0300

    change parameters of repository module

--- app/src/main/java/com/example/meditation_app/di/RepositoryModule.kt
+++ app/src/main/java/com/example/meditation_app/di/RepositoryModule.kt
@@ -2,8 +2,6 @@ package com.example.meditation_app.di
 
 import com.example.meditation_app.repo.AuthRepository
 import com.example.meditation_app.repo.AuthRepositoryImp
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,11 +15,7 @@ class RepositoryModule {
     @Provides
     @Singleton
     fun provideAuthRepository(
-        database: FirebaseFirestore,
-        auth: FirebaseAuth
-    ): AuthRepository {
-        return AuthRepositoryImp(auth, database)
-    }
-
+        authRepositoryImp: AuthRepositoryImp
+    ): AuthRepository = authRepositoryImp
 
 }
