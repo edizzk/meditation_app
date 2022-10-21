cd0d078134a6bd2ddc91a8bf19599ecab927446b
Ediz Mete Kurt <edizmetekurt@gmail.com>
2022-10-21 10:54:20 +0300

    create alert to use clickable condition strings

--- app/src/main/java/com/example/meditation_app/utils/Extensions.kt
+++ app/src/main/java/com/example/meditation_app/utils/Extensions.kt
@@ -1,7 +1,10 @@
 package com.example.meditation_app.utils
 
+import android.app.AlertDialog
+import android.content.Context
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.meditation_app.R
 
 
 fun Fragment.toast(msg: String?){
@@ -10,3 +13,13 @@ fun Fragment.toast(msg: String?){
 
 fun String.isValidEmail() =
     isNotEmpty() && android.util.Patterns.EMAIL_ADDRESS.matcher(this).matches()
+
+fun notAvailableAlert(context: Context){
+    val builder = AlertDialog.Builder(context)
+    builder.setTitle(UiString.StringResources(R.string.information).asString(context))
+    builder.setMessage(UiString.StringResources(R.string.not_availabe).asString(context))
+    builder.setPositiveButton(android.R.string.ok) { _, _ ->
+        Toast.makeText(context, android.R.string.ok, Toast.LENGTH_SHORT).show()
+    }
+    builder.show()
+}
