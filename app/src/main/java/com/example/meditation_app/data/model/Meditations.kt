3ba4aaa3445473083503112868ebbda84b800b61
Ediz Mete Kurt <edizmetekurt@gmail.com>
2022-10-31 09:43:40 +0300

    create models for stories and meditations

---
+++ app/src/main/java/com/example/meditation_app/data/model/Meditations.kt
@@ -0,0 +1,8 @@
+package com.example.meditation_app.data.model
+
+data class Meditations(
+    val id: Int? = 0,
+    val name: String? = "",
+    val subtitle: String? = "",
+    val desc: String? = ""
+)
---
+++ app/src/main/java/com/example/meditation_app/data/model/Stories.kt
@@ -0,0 +1,8 @@
+package com.example.meditation_app.data.model
+
+data class Stories (
+    val id: Int? = 0,
+    val name: String? = "",
+    val subtitle: String? = "",
+    val desc: String? = ""
+)
